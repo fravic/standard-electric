2f9701f70b2c44c831a030dbd796cb77b48c3345
Fravic <fravicf@gmail.com>
2025-02-16 18:29:52 -0800

    Cleanup

--- app/constants.ts
+++
@@ -1,6 +0,0 @@
-export const SESSION_TOKEN_COOKIE_KEY = "session-token";
-export const REFRESH_TOKEN_COOKIE_KEY = "refresh-token";
-
-// Time constants
-export const MILLISECONDS_PER_TICK = 5000; // 5 seconds per tick
-export const TICKS_PER_CYCLE = 12;
